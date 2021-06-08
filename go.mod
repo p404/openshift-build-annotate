6538b437c6308d45e9a75291808b34daa6a4f4f8
Pablo Opazo <paoc@Pablos-MacBook-Pro.local>
2021-06-08 11:15:06 -0700

    Test struct

--- go.mod
+++ go.mod
@@ -4,5 +4,9 @@ go 1.15
 
 require (
 	github.com/Jeffail/gabs v1.4.0
+	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
 	github.com/google/go-containerregistry v0.5.1
+	k8s.io/api v0.21.1 // indirect
+	k8s.io/klog v1.0.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v3 v3.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"net/http"
 	"time"
 	"html"
+	"io/ioutil"
+	"encoding/json"
 
 	"github.com/Jeffail/gabs"
 	"github.com/google/go-containerregistry/pkg/crane"
+	v1beta1 "k8s.io/api/admission/v1beta1"
+
 )
 
 type OpenShiftLabels struct {
@@ -43,8 +47,45 @@ func GetLabelsFromImage(image string) (*OpenShiftLabels, error) {
 	return ocl, nil
 }
 
+func sendError(err error, w http.ResponseWriter) {
+	log.Println(err)
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprintf(w, "%s", err)
+}
+
+func Mutate(body []byte) ([]byte, error) {
+	admReview := v1beta1.AdmissionReview{}
+
+	if err := json.Unmarshal(body, &admReview); err != nil {
+		return nil, fmt.Errorf("Unmarshaling request failed with %s", err)
+	}
+
+	ar := admReview.Request
+	fmt.Println(ar)
+	return body, nil
+}
+
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello %q", html.EscapeString(r.URL.Path))
+}
+
+func handleMutate(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+			sendError(err, w)
+			return
+	}
+
+	mutated, err := Mutate(body)
+	if err != nil {
+			sendError(err, w)
+			return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(mutated)
 }
 
 func main(){
@@ -52,6 +93,7 @@ func main(){
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
+	mux.HandleFunc("/mutate", handleMutate)
 
 	s := &http.Server{
 		Addr:           ":8443",
